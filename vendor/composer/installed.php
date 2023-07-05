d14f906ac7bf7986a9f7dbf6a4b6414b67a9b1b7
Syremios <aymeric.bourbier@etu-unistra.fr>
2023-07-05 10:39:01 +0200

    initiali commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c953268755621422b3faa88fa5b49aba4f72827d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c953268755621422b3faa88fa5b49aba4f72827d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.23',
+            'version' => '1.10.23.0',
+            'reference' => '65ab678d1248a8bc6fde456f0d7ff3562a61a4cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
